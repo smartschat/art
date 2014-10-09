27e4302671ea3880d13eabcc94f712d82e069b30
smartschat <sebastian.martschat@gmail.com>
2014-10-09 15:18:16 +0200

    Test bugfixing.

--- art/test/test_significance_tests.py
+++ art/test/test_significance_tests.py
@@ -1,8 +1,10 @@
 import os
 import unittest
 
-from art import significance_tests
 from art import aggregators
+from art import scores
+from art import significance_tests
+
 
 __author__ = 'smartschat'
 
@@ -11,8 +13,10 @@ class TestApproximateRandomizationTest(unittest.TestCase):
     def test_run(self):
         directory = os.path.dirname(os.path.realpath(__file__))
         test = significance_tests.ApproximateRandomizationTest(
-            open(directory + "/resources/example_scores"),
-            open(directory + "/resources/example_scores_numerator_always_0"),
+            scores.Scores.from_file(open(directory +
+                                         "/resources/example_scores")),
+            scores.Scores.from_file(open(
+                directory + "/resources/example_scores_numerator_always_0")),
             aggregators.enum_sum_div_by_denom_sum
         )
         self.assertGreater(test.run(), 0)
@@ -20,8 +24,10 @@ class TestApproximateRandomizationTest(unittest.TestCase):
     def test_run_with_same(self):
         directory = os.path.dirname(os.path.realpath(__file__))
         test = significance_tests.ApproximateRandomizationTest(
-            open(directory + "/resources/example_scores"),
-            open(directory + "/resources/example_scores"),
+            scores.Scores.from_file(open(directory +
+                                         "/resources/example_scores")),
+            scores.Scores.from_file(open(directory +
+                                         "/resources/example_scores")),
             aggregators.enum_sum_div_by_denom_sum
         )
         self.assertEqual(1.0, test.run())
